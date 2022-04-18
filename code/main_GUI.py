6c6035b45f79477253ccfe709d31792675486826
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-04-18 18:41:18 +0100

    Updated the GUI

--- code/main_GUI.py
+++ code/main_GUI.py
@@ -80,6 +80,58 @@ def search(table, searched_item, filter_obj=None):
     insert_media_table(table, filtered_data)
 
 
+
+
+# def suggestion_gui(root, account_data, account_found):
+#     utility.clear_root(root)
+#     title = tk.Label(root, text="VMedia: Suggestions", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
+#     title.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
+#     utility.underline(title)
+#
+#     table = create_table(root)
+#     table.place(relx=0.1, rely=0.2, relwidth=0.40, relheight=0.75)
+#     scroll_bar_y = tk.Scrollbar(root, command=table.yview)
+#     scroll_bar_y.place(relx=0.5, rely=0.2, relheight=0.75)
+#
+#     select_button = tk.Button(root, text="Select media", font=("arial", 10, "bold"),
+#                               bg=button_col, command=lambda:
+#                               select_media(root, table, likes_to_save,
+#                                            search_bar.get(), filter_obj))
+#     select_button.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+#
+#     search_bar = tk.Entry(root, relief=tk.GROOVE, bd=2, font=("arial", 13))
+#     search_bar.place(relx=0.1, rely=0.15, relwidth=0.28, relheight=0.025)
+#
+#     search_button = tk.Button(root, text="Search", font=("arial", 10, "bold"), bg=button_col,
+#                               command=lambda: search(table, search_bar.get(), filter_obj))
+#     search_button.place(relx=0.395, rely=0.15, relwidth=0.05, relheight=0.025)
+#
+#     show_all_button = tk.Button(root, text="Show All", font=("arial", 10, "bold"),
+#                                 bg=button_col, command=lambda: search(table, ""))
+#     show_all_button.place(relx=0.455, rely=0.15, relwidth=0.05, relheight=0.025)
+#
+#     exit_button = tk.Button(root, text="Exit", font=("arial", 10, "bold"),
+#                             bg=button_col, command=lambda: updating_account_data(account_found, likes_to_save))
+#     exit_button.place(relx=0.8, rely=0.05, relwidth=0.1, relheight=0.05)
+#
+#     filter_obj = Filters()
+#     filters(root, filter_obj)
+#
+#     print(len(account_data))
+#     if account_data == ['']:
+#         account_data = []
+#
+#     # A second list is made so media already used to calculate score do not need to be checked again
+#     likes_to_save: list[int] = [int(x) for x in account_data]
+#     global global_likes_to_save
+#     global_likes_to_save = likes_to_save
+#
+#     media_data_to_set_scores = suggestion_algorithm_single_use(likes_to_save)
+#
+#     ordered_media_classes = main_algorithm(media_data_to_set_scores)
+#     insert_media_table(table, ordered_media_classes)
+
+
 def suggestion_gui(root, account_data, account_found):
     utility.clear_root(root)
     title = tk.Label(root, text="VMedia: Suggestions", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
@@ -95,27 +147,30 @@ def suggestion_gui(root, account_data, account_found):
                               bg=button_col, command=lambda:
                               select_media(root, table, likes_to_save,
                                            search_bar.get(), filter_obj))
-    select_button.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+    select_button.place(relx=0.5, rely=0.1, relwidth=0.04, relheight=0.025, anchor=tk.CENTER)
 
     search_bar = tk.Entry(root, relief=tk.GROOVE, bd=2, font=("arial", 13))
     search_bar.place(relx=0.1, rely=0.15, relwidth=0.68, relheight=0.025)
 
     search_button = tk.Button(root, text="Search", font=("arial", 10, "bold"), bg=button_col,
                               command=lambda: search(table, search_bar.get(), filter_obj))
-    search_button.place(relx=0.8, rely=0.15, relwidth=0.05, relheight=0.025)
+    search_button.place(relx=0.8, rely=0.15, relwidth=0.04, relheight=0.025)
 
     show_all_button = tk.Button(root, text="Show All", font=("arial", 10, "bold"),
                                 bg=button_col, command=lambda: search(table, ""))
-    show_all_button.place(relx=0.86, rely=0.15, relwidth=0.05, relheight=0.025)
+    show_all_button.place(relx=0.85, rely=0.15, relwidth=0.04, relheight=0.025)
 
     exit_button = tk.Button(root, text="Exit", font=("arial", 10, "bold"),
                             bg=button_col, command=lambda: updating_account_data(account_found, likes_to_save))
-    exit_button.place(relx=0.8, rely=0.05, relwidth=0.1, relheight=0.05)
+    exit_button.place(relx=0.8, rely=0.05, relwidth=0.09, relheight=0.05)
+
+    filters_label = tk.Label(root, text="Filters:", font=("arial", 25, "bold"), fg=fg_col, bg=bg_col)
+    filters_label.place(relx=0.60, rely=0.25)
+    utility.underline(filters_label)
 
     filter_obj = Filters()
     filters(root, filter_obj)
 
-    print(len(account_data))
     if account_data == ['']:
         account_data = []
 
